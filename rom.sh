6c7beb28c11a37d72c8187980ff6b18dbe9de58f
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-11-03 19:35:17 +0200

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -1,35 +1,16 @@
 #!/bin/bash
-#
-# Copyright © 2019, "penglezos" <panagiotisegl@gmail.com>
-# Android ROM build script
-# Thanks to: @AliHasan7671, @kondors1995, @AryanKedare
-#
 
-tput reset
-echo -e "==============================================="
-echo    "         Compiling Englezos ROM                "
-echo -e "==============================================="
-
-
-# Syncing
-echo -e "Syncing from sources."
 repo init -u git://github.com/LineageOS/android.git -b lineage-16.0
 repo sync -f --force-sync --no-tags --no-clone-bundle
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git packages/resources/devicesettings
-echo -e "Syncing finished."
 
-# Initialize device trees
-echo -e "Initializing device: (device tree, vendor, kernel)."
-git clone https://github.com/penglezos/android_device_xiaomi_mido -b lineage-16.0 device/xiaomi/mido
-git clone https://github.com/zeelog/proprietary_vendor_xiaomi -b lineage-16.0 vendor/xiaomi
-git clone https://github.com/penglezos/android_kernel_xiaomi_msm8953 -b pie kernel/xiaomi/msm8953
-echo -e "Finished successfully."
+git clone https://github.com/TheScarastic/android_device_xiaomi_raphael.git -b lineage-16.0 device/xiaomi/raphael
+git clone https://github.com/TheScarastic/proprietary_vendor_xiaomi.git -b lineage-16.0 vendor/xiaomi
 
-# Compiling
 source build/envsetup.sh
 export USE_CCACHE=1
 export CCACHE_DIR=~/.ccache
 prebuilts/misc/linux-x86/ccache/ccache -M 50G
-lunch lineage_mido-userdebug
+lunch lineage_raphael-userdebug
 mka installclean
 make bacon -j$(nproc --all)
