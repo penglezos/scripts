8db6fa21c16f3d2cdb1474d6def030eb40697418
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-11-23 17:28:36 +0200

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -4,8 +4,8 @@ repo init -u git://github.com/LineageOS/android.git -b lineage-16.0
 repo sync -f --force-sync --no-tags --no-clone-bundle
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git packages/resources/devicesettings
 
-git clone https://github.com/TheScarastic/android_device_xiaomi_raphael.git -b lineage-16.0 device/xiaomi/raphael
-git clone https://github.com/TheScarastic/proprietary_vendor_xiaomi.git -b lineage-16.0-raphael vendor/xiaomi
+git clone https://github.com/raphael-development/android_device_xiaomi_raphael -b lineage-17.0 device/xiaomi/raphael
+git clone https://github.com/raphael-development/proprietary_vendor_xiaomi -b lineage-17.0 vendor/xiaomi
 
 source build/envsetup.sh
 export USE_CCACHE=1
