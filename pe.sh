77f0a197e9d665c43c81bc2d8e975f69d52b4fe4
Panagiotis Englezos <panagiotisegl@gmail.com>
2019-12-05 11:26:03 +0200

    Create pe.sh

---
+++ pe.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+repo init -u https://github.com/PixelExperience/manifest -b ten
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+. build/envsetup.sh
+lunch aosp_raphael-userdebug
+mka bacon -j8
