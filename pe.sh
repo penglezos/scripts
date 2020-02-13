71416e4bcd64d91799158ed3d9a2be9c91431f98
Panagiotis Englezos <panagiotisegl@gmail.com>
2020-02-13 19:41:23 +0200

    Update pe.sh to a more dynamic setup

--- pe.sh
+++ pe.sh
@@ -1,7 +1,51 @@
 #!/bin/bash
 
-repo init -u https://github.com/PixelExperience/manifest -b ten
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+# Sync ROM Sources
+while read -p "Do you want to sync (y/n)? " cchoice
+do
+case "$cchoice" in
+	y|Y )
+		repo init -u https://github.com/PixelExperience/manifest -b ten
+        repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+		echo
+		echo "Synced successfully!"
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+
+# Sync with my changes
+while read -p "Do you want to sync your changes (y/n)? " cchoice
+do
+case "$cchoice" in
+	y|Y )
+		rm -rf device/xiaomi/
+		git clone https://github.com/penglezos/device_xiaomi_raphael -b wip device/xiaomi/raphael
+		echo
+		echo "Synced successfully!"
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+# Compile
 . build/envsetup.sh
 lunch aosp_raphael-userdebug
 make bacon -j$(nproc --all)
