4aa7dcb23cea6173b7593eb429e66f7641266be3
Panagiotis Englezos <panagiotisegl@gmail.com>
2020-03-15 15:18:57 +0200

    Update kernel_clang.sh

--- kernel_clang.sh
+++ kernel_clang.sh
@@ -6,6 +6,10 @@ echo -e "==============================================="
 echo    "         Compiling Englezos Kernel             "
 echo -e "==============================================="
 
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER=penglezos
+export KBUILD_BUILD_HOST=box
 export CLANG_PATH=/home/home/tools/clang/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
@@ -18,11 +22,6 @@ REPACK_DIR=$KERNEL_DIR/AnyKernel3
 OUT=$KERNEL_DIR/out
 DATE=`date +"%Y%m%d-%H%M"`
 DATE_START=$(date +"%s")
-
-export ARCH=arm64
-export SUBARCH=arm64
-export KBUILD_BUILD_USER=penglezos
-export KBUILD_BUILD_HOST=box
 
 rm -rf out
 make O=out CC=clang raphael_defconfig
