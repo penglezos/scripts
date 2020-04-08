a67f2fb53553d5f1647d42806663ec375112c331
Panagiotis Englezos <panagiotisegl@gmail.com>
2020-04-09 00:15:01 +0300

    sync

--- kernel_clang.sh
+++ kernel_clang.sh
@@ -12,12 +12,11 @@ export ARCH=arm64
 export SUBARCH=arm64
 export CLANG_PATH=${HOME}/tools/clang/bin
 export PATH=${CLANG_PATH}:${PATH}
-export KBUILD_COMPILER_STRING=$($CLANG_PATH/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=${HOME}/tools/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=${HOME}/tools/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 export LD_LIBRARY_PATH=${HOME}/tools/clang/aosp/clang/lib64:$LD_LIBRARY_PATH
-VERSION='r1'
+VERSION='0'
 KERNEL_DIR=`pwd`
 REPACK_DIR=$KERNEL_DIR/AnyKernel3
 OUT=$KERNEL_DIR/out
@@ -30,7 +29,7 @@ make O=out CC=clang -j$(grep -c ^processor /proc/cpuinfo)
 
 cd $REPACK_DIR
 cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb $REPACK_DIR/
-FINAL_ZIP="EnglezosKernel-${VERSION}.zip"
+FINAL_ZIP="Englezos-raphael-${VERSION}.zip"
 zip -r9 "${FINAL_ZIP}" *
 cp *.zip $OUT
 rm *.zip
