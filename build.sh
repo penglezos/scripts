9449b61b3d83c99730ec87e078cb40da982037a4
Panagiotis Englezos <panagiotisegl@gmail.com>
2020-06-19 23:31:58 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=${HOME}/tools/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=${HOME}/tools/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 export LD_LIBRARY_PATH=${HOME}/tools/clang/lib64:$LD_LIBRARY_PATH
+LC_ALL=C date +%Y-%m-%d	
+date=`date +"%Y%m%d-%H%M"`	
+BUILD_START=$(date +"%s")
 VERSION='0.4'
 KERNEL_DIR=`pwd`
 REPACK_DIR=$KERNEL_DIR/AK
@@ -24,4 +27,7 @@ cp *.zip $OUT
 rm *.zip
 cd $KERNEL_DIR
 rm AK/Image.gz-dtb
-echo "Done!"
+
+BUILD_END=$(date +"%s")	
+DIFF=$(($BUILD_END - $BUILD_START))	
+echo -e "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
