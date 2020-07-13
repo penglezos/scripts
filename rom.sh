2db60253368a86063a4522f104492b956844abeb
Panagiotis Englezos <panagiotisegl@gmail.com>
2020-07-13 23:56:54 +0300

    Create rom.sh

---
+++ rom.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+repo init -u git://github.com/LineageOS/android.git -b lineage-17.1
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+git clone https://github.com/penglezos/device_xiaomi_raphael -b lineage-17.1 device/xiaomi/raphael
+git clone https://github.com/raphael-development/proprietary_vendor_xiaomi vendor/xiaomi/
+
+. build/envsetup.sh
+lunch lineage_raphael-userdebug
+export LC_ALL=C
+make bacon -j$(nproc --all)
