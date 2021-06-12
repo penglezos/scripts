02195002ec27844fece7b31329df14abd49a0b09
Panagiotis Englezos <panagiotisegl@gmail.com>
2021-06-12 13:29:44 +0300

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -4,13 +4,14 @@
 git config --global user.name "penglezos"
 git config --global user.email "panagiotisegl@gmail.com"
 
-# Initialize LineageOS and device files.
+# Initialize LineageOS source code.
 repo init -u git://github.com/LineageOS/android.git -b lineage-18.1
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+# Initialize device files.
 git clone https://github.com/penglezos/device_xiaomi_raphael device/xiaomi/raphael
 git clone https://github.com/penglezos/vendor_xiaomi_raphael vendor/xiaomi/raphael
 git clone https://github.com/penglezos/kernel_xiaomi_raphael kernel/xiaomi/raphael
-
 
 # Set build enviroment and build.
 ccache -M 50G
