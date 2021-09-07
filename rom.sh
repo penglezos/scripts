d6b964f85ee9b1edc62355ca7d22c228fa3d1568
Panagiotis Englezos <panagiotisegl@gmail.com>
2021-09-07 18:46:18 +0300

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -9,10 +9,9 @@ repo init -u git://github.com/LineageOS/android.git -b lineage-18.1
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # Initialize device files.
-git clone https://github.com/penglezos/device_xiaomi_raphael device/xiaomi/raphael
-git clone https://github.com/penglezos/vendor_xiaomi_raphael vendor/xiaomi/raphael
+git clone https://github.com/EnglezosRaphael/device_xiaomi_raphael device/xiaomi/raphael
+git clone https://github.com/EnglezosRaphael/vendor_xiaomi_raphael vendor/xiaomi/raphael
 git clone https://github.com/penglezos/kernel_xiaomi_raphael kernel/xiaomi/raphael
-git clone https://github.com/penglezos/device_xiaomi_raphael-sepolicy device/xiaomi/raphael-sepolicy
 
 # Set build enviroment and build.
 ccache -M 50G
