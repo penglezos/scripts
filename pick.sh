614d44dd6361e70cf34f426e0c1d87f1219e28e7
Panagiotis Englezos <panagiotisegl@gmail.com>
2021-10-15 18:17:20 +0300

    pick: Add comments to identify changes

--- pick.sh
+++ pick.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source build/envsetup.sh
-repopick 312889 312890 312891
-repopick 318057
-repopick 318058
+repopick 312889 312890 312891 # burnin protection
+repopick 318057 # blur into display section
+repopick 318058 # nfc icon on status bar
 
