5a97073983554c3a78c47eb6c7c55ee4e87a7731
Panagiotis Englezos <panagiotisegl@gmail.com>
2021-11-06 22:16:45 +0200

    Android 12

--- rom.sh
+++ rom.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+export USERNAME=penglezos
+export HOSTNAME=home
+
 # Setup Git profile.
 git config --global user.name "penglezos"
 git config --global user.email "panagiotisegl@gmail.com"
 
 # Initialize LineageOS source code.
-repo init -u git://github.com/LineageOS/android.git -b lineage-18.1
+repo init -u git://github.com/LineageOS/android.git -b lineage-19.0
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # Initialize device files.
@@ -14,10 +17,10 @@ git clone https://github.com/EnglezosRaphael/vendor_xiaomi_raphael vendor/xiaomi
 git clone https://github.com/EnglezosRaphael/kernel_xiaomi_raphael kernel/xiaomi/raphael
 
 # Set build enviroment and build.
-ccache -M 50G
+ccache -M 100G
 export USE_CCACHE=1
 export CCACHE_EXEC=$(command -v ccache)
-. build/envsetup.sh
-lunch lineage_raphael-userdebug
 export LC_ALL=C
-make bacon
+source build/envsetup.sh
+lunch lineage_raphael-userdebug
+make bacon -j16
