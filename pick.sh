93e07f3e5e1c697983414fe432fabf333a5d8b7d
Panagiotis Englezos <panagiotisegl@gmail.com>
2021-11-10 16:00:47 +0200

    Pick monet changes

--- pick.sh
+++ pick.sh
@@ -4,3 +4,4 @@ repopick 312889 312890 312891 # burnin protection
 repopick 318057 # blur into display section
 repopick 318058 # nfc icon on status bar
 repopick -f 318301 # always use release-keys
+repopick -t twelve-monet # monet support
