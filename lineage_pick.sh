02f68d5ca2469d6240762b0e2717f029e1f3a856
Panagiotis Englezos <panagiotisegl@gmail.com>
2021-12-02 16:18:02 +0200

    Delete lineage_pick.sh

--- lineage_pick.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source build/envsetup.sh
-#repopick 312889 312890 312891 # burnin protection
-#repopick 318057 # blur into display section
-#repopick 318058 # nfc icon on status bar
-#repopick -f 318301 # always use release-keys
-repopick -t twelve-monet # monet support
