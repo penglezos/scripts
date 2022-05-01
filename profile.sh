d481448460b458d8bf04fe4ffaadb261e51154ae
penglezos <panagiotisegl@gmail.com>
2022-05-02 00:48:36 +0300

    profile: Add git alias

--- profile.sh
+++ profile.sh
@@ -2,3 +2,12 @@
 
 git config --global user.name "penglezos"
 git config --global user.email "panagiotisegl@gmail.com"
+
+git config --global alias.cp 'cherry-pick'
+git config --global alias.c 'commit'
+git config --global alias.f 'fetch'
+git config --global alias.rb 'rebase'
+git config --global alias.rs 'reset'
+git config --global alias.ck 'checkout'
+git config --global alias.r 'revert'
+git config --global alias.rc 'revert --no-commit'
