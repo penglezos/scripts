523efe251be34b6683026abead1192764b78f2f7
Panagiotis Englezos <panagiotisegl@gmail.com>
2022-07-03 00:34:12 +0300

    setup: cleanup git alias configuration

--- setup.sh
+++ setup.sh
@@ -7,14 +7,8 @@ git config --global review.review.arrowos.net.username "penglezos"
 
 # Git alias configuration
 git config --global alias.cp 'cherry-pick'
-git config --global alias.c 'commit'
-git config --global alias.f 'fetch'
-git config --global alias.rb 'rebase'
-git config --global alias.rs 'reset'
-git config --global alias.ck 'checkout'
 git config --global alias.r 'revert'
 git config --global alias.rc 'revert --no-commit'
-git config --global alias.p 'push'
 
 # Install necessary packages
 sudo pacman -S firefox git code htop neofetch 
