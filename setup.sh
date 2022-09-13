742f2645642cfaef6dae484c8836acefbab9cfce
Panagiotis Englezos <panagiotisegl@gmail.com>
2022-09-13 19:43:24 +0300

    Bluetooth setup

--- setup.sh
+++ setup.sh
@@ -11,7 +11,11 @@ git config --global alias.r 'revert'
 git config --global alias.rc 'revert --no-commit'
 
 # Install necessary packages
-sudo pacman -S firefox git code htop neofetch telegram-desktop
+sudo pacman -S bluez bluez-utils blueman firefox git code htop neofetch telegram-desktop
+
+# Enable bluetooth service
+sudo systemctl start bluetooth.service
+sudo systemctl enable bluetooth.service
 
 # Install yay
 git clone https://aur.archlinux.org/yay.git
