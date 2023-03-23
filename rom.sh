d2f68cefaafc6c6eb9b2abf4cc4d30786b2da36f
penglezos <panagiotisegl@gmail.com>
2023-03-23 20:08:57 +0200

    rom: Remove apply patches function
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- rom.sh
+++ rom.sh
@@ -13,7 +13,7 @@ echo -e "==============================================="
 echo    "         Android Build script                  "
 echo -e "==============================================="
 echo -e
-echo -e "Available options:\n1.Sync ROM\n2.Pick patches\n3.Set zram\n4.Build ROM\n5.Build Kernel\n6.Build recovery"
+echo -e "Available options:\n1.Sync ROM\n2.Set zram\n3.Build ROM\n4.Build Kernel\n5.Build recovery"
 echo -e
 read -p "Your choice: " num
 case $num in 
@@ -25,15 +25,12 @@ if [ $num = '1' ]; then
     repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
     
     elif [ $num = '2' ]; then
-    ./apply.sh
-    
-    elif [ $num = '3' ]; then
     sudo swapoff --all
     sudo bash -c "echo 32G > /sys/block/zram0/disksize"
     sudo mkswap --label zram0 /dev/zram0
     sudo swapon --priority 32767 /dev/zram0
 
-    elif [ $num = '4' ]; then
+    elif [ $num = '3' ]; then
     export CCACHE_EXEC=$(command -v ccache)
     export CCACHE_DIR=$(pwd)/.ccache
     export USE_CCACHE=1
@@ -43,13 +40,13 @@ if [ $num = '1' ]; then
     lunch lineage_${device}-${build_type}
     make bacon
     
-    elif [ $num = '5' ]; then
+    elif [ $num = '4' ]; then
     source build/envsetup.sh
     lunch lineage_${device}-${build_type}
     make bootimage
     mv boot.img englezos-${date}-kernel-${device}.img
 
-    elif [ $num = '6' ]; then
+    elif [ $num = '5' ]; then
     source build/envsetup.sh
     lunch lineage_${device}-${build_type}
     make recoveryimage
