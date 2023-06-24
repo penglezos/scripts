f6d133f5469d7f43ae9efbc014482cec63cc8cea
penglezos <panagiotisegl@gmail.com>
2023-06-24 13:12:27 +0300

    Add build_soong patches
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- patches.sh
+++ patches.sh
@@ -5,3 +5,9 @@ cd build/core
 git fetch https://github.com/EnglezosAOSP/android_build/
 git cherry-pick 5e9b696
 cd ../../
+
+# build_soong
+cd build/soong
+git fetch https://github.com/EnglezosAOSP/android_build_soong/
+git cherry-pick 7e07da0^..82153b8
+cd ../../
