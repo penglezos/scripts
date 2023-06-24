3065e268c83bd33ac77f5eaf7705c1ee2399fec4
penglezos <panagiotisegl@gmail.com>
2023-06-24 13:16:29 +0300

    Add lineage-sdk patches
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- patches.sh
+++ patches.sh
@@ -11,3 +11,9 @@ cd build/soong
 git fetch https://github.com/EnglezosAOSP/android_build_soong/
 git cherry-pick 7e07da0^..82153b8
 cd ../../
+
+# lineage-sdk
+cd lineage-sdk
+git fetch https://github.com/EnglezosAOSP/android_lineage-sdk
+git cherry-pick bb78702
+cd ../../
