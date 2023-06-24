b3bdfbee2f3685d7f06858fb5906962a5f5f02cd
penglezos <panagiotisegl@gmail.com>
2023-06-24 12:18:17 +0300

    Rewrite build script
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- build.sh
+++ build.sh
@@ -9,6 +9,44 @@ device='raphael'
 build_type='userdebug'
 date=`date +"%Y%m%d"`
 
+sync () {
+    repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
+    rm -rf .repo/local_manifests
+    mkdir -p .repo/local_manifests
+    curl https://raw.githubusercontent.com/penglezos/local_manifests/lineage-20/raphael.xml -o .repo/local_manifests/lineage.xml
+    repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+}
+
+setup () {
+    sudo swapoff --all
+    sudo bash -c "echo 32G > /sys/block/zram0/disksize"
+    sudo mkswap --label zram0 /dev/zram0
+    sudo swapon --priority 32767 /dev/zram0
+}
+
+build () {
+    export CCACHE_EXEC=$(command -v ccache)
+    export CCACHE_DIR=$(pwd)/.ccache
+    export USE_CCACHE=1
+    ccache -M 50G
+    export LC_ALL=C
+    source build/envsetup.sh
+    lunch lineage_${device}-${build_type}
+    make bacon
+}
+
+kernel () {
+    source build/envsetup.sh
+    lunch lineage_${device}-${build_type}
+    make bootimage
+}
+
+recovery () {
+    source build/envsetup.sh
+    lunch lineage_${device}-${build_type}
+    make recoveryimage
+}
+
 echo -e "==============================================="
 echo    "         Android Build script                  "
 echo -e "==============================================="
@@ -21,42 +59,17 @@ case $num in
 esac
 
 if [ $num = '1' ]; then
-    repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
-    rm -rf .repo/local_manifests
-    mkdir -p .repo/local_manifests
-    curl https://raw.githubusercontent.com/penglezos/local_manifests/lineage-20/raphael.xml -o .repo/local_manifests/lineage.xml
-    repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+    sync
+elif [ $num = '2' ]; then
+    ./patches.sh
+elif [ $num = '3' ]; then
+    setup
 
-    elif [ $num = '2' ]; then
-    ./apply.sh
-
-    elif [ $num = '3' ]; then
-    sudo swapoff --all
-    sudo bash -c "echo 32G > /sys/block/zram0/disksize"
-    sudo mkswap --label zram0 /dev/zram0
-    sudo swapon --priority 32767 /dev/zram0
-
-    elif [ $num = '4' ]; then
-    export CCACHE_EXEC=$(command -v ccache)
-    export CCACHE_DIR=$(pwd)/.ccache
-    export USE_CCACHE=1
-    ccache -M 50G
-    export LC_ALL=C
-    source build/envsetup.sh
-    lunch lineage_${device}-${build_type}
-    make bacon
-    
-    elif [ $num = '5' ]; then
-    source build/envsetup.sh
-    lunch lineage_${device}-${build_type}
-    make bootimage
-    mv boot.img englezos-${date}-kernel-${device}.img
-
-    elif [ $num = '6' ]; then
-    source build/envsetup.sh
-    lunch lineage_${device}-${build_type}
-    make recoveryimage
-    mv recovery.img lineage-20.0-${date}-recovery-${device}.img
-
-    else echo "Invalid input, aborting!"
+elif [ $num = '4' ]; then
+    build
+elif [ $num = '5' ]; then
+    kernel
+elif [ $num = '6' ]; then
+    recovery
+else echo "Invalid input, aborting!"
 fi
