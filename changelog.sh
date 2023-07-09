ee341b6af03daf2193708fd08f87916fcb462726
penglezos <panagiotisegl@gmail.com>
2023-07-09 14:28:06 +0300

    changelog: Hardcode out directory
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- changelog.sh
+++ changelog.sh
@@ -62,5 +62,5 @@ done
 
 sed -i 's/project/   */g' $Changelog
 
-cp $Changelog $OUT_DIR/target/product/$DEVICE/system/etc/
-mv $Changelog $OUT_DIR/target/product/$DEVICE/
+cp $Changelog out/target/product/$DEVICE/system/etc/
+mv $Changelog out/target/product/$DEVICE/
