067b08a2ccc5739a8c8cac4a9cdfc332f256ec06
penglezos <panagiotisegl@gmail.com>
2023-10-29 00:09:54 +0300

    Code style improvement
    Signed-off-by: penglezos <panagiotisegl@gmail.com>

--- build.sh
+++ build.sh
@@ -10,8 +10,7 @@ build_type='userdebug'
 
 sync () {
     repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
-    rm -rf .repo/local_manifests
-    mkdir -p .repo/local_manifests
+    rm -rf .repo/local_manifests && mkdir -p .repo/local_manifests
     curl https://raw.githubusercontent.com/penglezos/local_manifests/lineage-20/raphael.xml -o .repo/local_manifests/lineage.xml
     repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 }
